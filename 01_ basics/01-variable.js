bd70bf3b5892bf8514a7eea802e03cce573b093e
Bhavyansh Sangwani <bhavyanshsangwani@gmail.com>
2025-06-09 13:27:12 +0000

    datatypes

---
+++ 01_ basics/01-variable.js
@@ -0,0 +1,11 @@
+const accountId=15641
+let account ="bhavyansh@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+
+account="djfhnjdf@gmail.com"
+accountPassword="54353"
+accountCity="Mumbai"
+console.log(accountId);
+console.table([accountId,account,accountPassword,accountCity]);
---
+++ 01_ basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict";// to Treat all JS code as newer version
+
+//alert(3+3)//we r using node_js not browser
+
+
+let name="Bhavyansh"
+let age =21
+let isLoggedIn=false
+
+/*
+number=2 to power 53
+bigint
+String=>""
+boolean=>true/false
+null=>standalone value
+undefined=>
+symbol=>unique
+*/
+
+console.log(typeof"Bhavyansh")   //String
+console.log(typeof age)         //number
+console.log(typeof null)       //object
+console.log(typeof undefined) //undefined
