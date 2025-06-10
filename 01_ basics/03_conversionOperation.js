b0d1559d882b0e41f61c2654138475cf6ce30332
Bhavyansh Sangwani <bhavyanshsangwani@gmail.com>
2025-06-10 08:44:40 +0000

    Conversion of Data types

---
+++ 01_ basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+let score="33jjx";
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
+
+//"33"=>33
+//"33fes"=>NAN
+//true=>1,false=>0
+//""=>false, 
+//"bhavyansh"=>true
+
+let loggedin=1
+let booleanLoggedIn=Boolean(loggedin);
+console.log(booleanLoggedIn)
