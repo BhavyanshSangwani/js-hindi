4cd0403ddf94b86bc8c5a5a658f52d019f6c5773
Bhavyansh Sangwani <bhavyanshsangwani@gmail.com>
2025-06-17 13:01:44 +0000

    07_datesIn.js

---
+++ 01_ basics/07_datesIn.js
@@ -0,0 +1,27 @@
+//Dates
+// let myDate=new Date();
+// console.log (myDate.toString())
+// console.log (myDate.toDateString())
+// console.log (myDate.toLocaleString())
+// console.log (typeof myDate)
+
+//  let myCreatedDate=new Date(2023,0,23)
+// console.log(myCreatedDate.toDateString());
+// //month startes vith the value=0 & ends on value=11
+
+
+ let myCreatedDate=new Date("2023-01-23")
+// //console.log(myCreatedDate1.toDateString());
+ //let myTimeStamp=Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate1.getTime());
+//console.log(math.floor(Date.now()/1000))
+//  let myCreatedDate2=new Date("21-2-2023")
+ let myTimeStamp=date.now()
+// console.log(math.floor(Date.now()/1000))
+ 
+let newDate=myCreatedDate
+console.log(newDate);
+console.log(newDate.getMonth())
+
+
